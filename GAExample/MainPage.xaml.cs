ff0e0f68fec259d029f8ab77f1733ab1e76a1846
Ilan Olkies <ilanolkies@outlook.com>
2018-05-31 00:20:36 -0300

    Add Events page as a TabbedPage.

--- GAExample/MainPage.xaml.cs
+++ GAExample/MainPage.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace GAExample
 {
-    public partial class MainPage : ContentPage
+	public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
-            InitializeComponent();
+			InitializeComponent();
         }
     }
 }
---
+++ GAExample/Pages/EventsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace GAExample.Pages
+{
+    public partial class EventsPage : ContentPage
+    {
+        public EventsPage()
+        {
+			InitializeComponent();
+
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+        }
+    }
+}
